0c3c641e967e113157b0f434c5b4e6ca81830cd5
badrEladraoui1 <eladraoui.badr@gmail.com>
2023-11-23 11:52:36 +0100

    adding class librairies

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAcess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Samsung", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Iphone", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "OPPO", DisplayOrder = 3 }
+                );
+        }
+
+    }
+}
