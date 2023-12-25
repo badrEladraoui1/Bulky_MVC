12bb1955384be28af9a1015531c2c4c5b438f935
badrEladraoui1 <eladraoui.badr@gmail.com>
2023-12-25 09:55:54 +0100

    fixing user roles

---
+++ Bulky.Utility/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Bulky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        Task IEmailSender.SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic to send email in the futur
+            return Task.CompletedTask;
+        }
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,17 +1,33 @@
 using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.DataAcess.Data;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+
+});
+
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
@@ -27,8 +43,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
